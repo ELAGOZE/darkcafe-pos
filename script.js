d444bea8bc0f33d0453534f8292b4c27eee1998c
ELAGOZE <elagoze56@gmail.com>
2025-10-14 17:51:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-const channel = new BroadcastChannel('darkcafe_channel');
-
 function sendToGoogleSheet(item) {
-  fetch(https://script.google.com/macros/s/AKfycbzDLLKhJg1-5upRYC3JJBmHBOQm37L_qWwuLXrQYod7y2cXRmdzf_-CzYfNe6EL4mfS/exec', {
+  fetch('https://script.google.com/macros/s/AKfycbzDLLKhJg1-5upRYC3JJBmHBOQm37L_qWwuLXrQYod7y2cXRmdzf_-CzYfNe6EL4mfS/exec', {
     method: 'POST',
     body: JSON.stringify(item),
     headers: {'Content-Type': 'application/json'}
   })
   .then(res => console.log('✅ تم الإرسال للـ Sheet'))
   .catch(err => console.error('❌ حصل خطأ:', err));
-
-  channel.postMessage(item);
 }
 
 function saveOrder() {
